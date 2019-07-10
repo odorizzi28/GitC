03ba6a4d78dc7d89e53aa1d18da688bf7fa7759c
Jalin Habei <Jalin.Habei@gmail.com>
2019-07-10 10:56:23 -0300

    Array de duas posicoes

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] listaDeLivros = new string[2, 2]
+                {{"O pequeno","sim"},{"O grande", "Não"}};
+
+            for (int i = 0; i < listaDeLivros.GetLength(0); i++)
+            {
+                var Nomelivro = listaDeLivros[i,0];
+                var disponivel = listaDeLivros[i, 1];
+
+                Console.WriteLine($"Nome do livro:{Nomelivro} disponivel:{disponivel}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
